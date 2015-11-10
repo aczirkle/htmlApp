33d26d0eec7f3ee7153836fccdd1d41c1afaa832
aczirkle <aczirkle@gmail.com>
2015-11-09 21:00:29 -0500

    sadfsdafa

--- WEB-INF/classes/MVC.java
+++ WEB-INF/classes/MVC.java
@@ -56,7 +56,7 @@ public class MVC extends HttpServlet {
 		catch (Exception e){e.printStackTrace(out);}
 	}
 	
-	private void errorPage(HttpServletRequest request, HttpServletResponse response){
+	private void errorPage(HttpServletRequest request, HttpServletResponse response) throws ServletException{
 		PrintWriter out = response.getWriter();
 		out.println("I ran into an error.");
 		out.println("You requested "+request.getServletPath());
