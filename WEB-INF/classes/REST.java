c20bdffc8cd14980375872b4544e1867f66a9cab
aczirkle <aczirkle@gmail.com>
2015-11-17 20:46:46 -0500

    as arrays

--- WEB-INF/classes/REST.java
+++ WEB-INF/classes/REST.java
@@ -108,7 +108,7 @@ public class REST {
 		int count= ar.size();
 		
 		JSONObject jo = new JSONObject();
-		jo.put("pages",new JSONArray(ar));
+		jo.put("pages",new JSONArray(Arrays.asList(ar)));
 		jo.put("author","none");
 		jo.put("title",name);
 		jo.put("numPages", count);
